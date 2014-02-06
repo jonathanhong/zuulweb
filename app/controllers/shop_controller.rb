7212a1e949841f83144507c5d7293849cdeb3083
Jonathan Hong <Jonathan.Hong8@gmail.com>
2014-02-06 15:29:31 -0800

    added purchase route and pseudocode for shop controller

--- app/controllers/shop_controller.rb
+++ app/controllers/shop_controller.rb
@@ -4,4 +4,14 @@ class ShopController < ApplicationController
     @items = Item.where(active: true)
     @item_count = @items.size
   end
+  
+  def purchase
+	#retrieve POST data [{"itemids" : {array, of, itemids}, userid}]
+	#UPDATE quantity in item table
+	#SELECT item prices from item table
+	#UPDATE balance in user table
+	#ADD log in transactions table
+	
+	#return success or fail code
+  end
 end
