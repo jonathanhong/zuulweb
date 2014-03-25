18fce82364648c4486774e93efb27e33eca997e2
Jonathan Hong <Jonathan.hong8@gmail.com>
2014-03-24 22:07:19 -0400

    Changed shop CoffeeScript to JavaScript

--- app/assets/javascripts/shop.js
+++ app/assets/javascripts/shop.js
@@ -1,6 +1,4 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://coffeescript.org/
+/* ZuulWeb 2.0 Shop Index shop.js JavaScript Document */
 
 $(function() {
 	$(".plus").click(function() {
