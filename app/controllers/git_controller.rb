09f6eebf2b137be05b61273f910b52fe069e93f6
Chance Snow <chances@cat.pdx.edu>
2014-03-24 20:55:15 -0700

    Added logging to git controller

--- app/controllers/git_controller.rb
+++ app/controllers/git_controller.rb
@@ -9,22 +9,30 @@ class GitController < ApplicationController
                        :except => :deploy
 
   def deploy
+    log = Logger.new(open("#{ENV['ZUUL']}/log/deploy.log", 'a'))
+    log.info('Deploying application')
     begin
+      log.info('Recieved payload')
       github_json = JSON.parse(params[:payload])
       #Ensure this is a GitHub post thing
       repo = github_json['repository']
       if repo['private'] == true and repo['name'] == 'zuulweb' and repo['language'] == 'Ruby'
+        log.info('Payload is authentic')
+	log.info('Pulling remote branches...')
         cmd = 'for remote in `git branch -r`; do git branch --track $remote; done'
         `#{cmd}`
         #`git fetch --all`
         `git pull --all`
+	log.info('Restarting application...')
         FileUtils.touch("#{ENV['ZUUL']}/tmp/restart.txt")
+	log.info('Deployed')
         head :ok, :content_type => 'text/plain'
       else
+        log.error('Payload is NOT authentic')
         redirect_to request.host
       end
-    rescue Exception
-      #TODO: Error log?
+    rescue Exception => e
+      log.fatal("ERROR: #{e.message}")
     end
   end
 
@@ -34,12 +42,13 @@ class GitController < ApplicationController
       @branches = g.branches
     rescue Exception
       @branches = []
-      #TODO: Error log?
     end
   end
 
   def checkout
+    log = Logger.new(open("#{ENV['ZUUL']}/log/deploy.log", 'a'))
     begin
+      log.info("Attempting to checkout changes on branch #{params[:branch]}")
       g = Git.open(ENV['ZUUL'])
       exists = false
       g.branches.each do |branch|
@@ -47,15 +56,22 @@ class GitController < ApplicationController
           exists = true
           break
         end
-      end 
-      g.checkout params[:branch] if exists
-      FileUtils.touch("#{ENV['ZUUL']}/tmp/restart.txt") if exists
-      @success = exists
+      end
+      log.fatal("ERROR: Branch '#{params[:branch]}' does not exist and cannot be checked out") unless exists
+      if exists
+        g.checkout params[:branch]
+	log.info('Pulling remote changes...')
+	`git pull origin #{params[:branch]}`
+        log.info('Restarting application...')
+        FileUtils.touch("#{ENV['ZUUL']}/tmp/restart.txt")
+	log.info("Successfully checked out changes on branch #{params[:branch]}")
+        @success = exists
+      end
       @message = exists ? "Successfuly checked out branch '#{params[:branch]}'." : "Branch '#{params[:branch]}' was not found."
     rescue Exception => e
       @success = false
       @message = e.message
-      #TODO: Error log? (e.backtrace.inspect)
+      log.fatal("ERROR: #{e.message}")
     end
   end
 end
