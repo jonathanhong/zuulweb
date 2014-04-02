bac690ceb661296dca0cd9a3e6a13afc49395807
Jonathan Hong <Jonathan.Hong8@gmail.com>
2014-04-02 16:42:31 -0700

    added some initial javascript/css to profile

---
+++ app/assets/javascripts/profile.js
@@ -0,0 +1,15 @@
+# Place all the behaviors and hooks related to the matching controller here.
+# All this logic will automatically be available in application.js.
+# You can use CoffeeScript in this file: http://coffeescript.org/
+function expand() {
+	this.innerHTML = '<span class="glyphicon glyphicon-minus"></span>';
+	#TODO: expand functionality here
+}
+
+window.onload = function() {
+	var expandBtns = document.getElementsByClass("expand");
+	for (var i = 0; i < expandButtons.length; i++) {
+		expandBtns[i].onclick = expand();
+		}
+	}
+}
--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,5 +1,6 @@
 class ProfileController < ApplicationController
   def index
+		#TODO: add user auth
 		@user = User.find_by username: 'jhong'
 		@transactions = Transaction.where("userid = ?", @user.id).limit(5).order('created_at desc')	
   end
