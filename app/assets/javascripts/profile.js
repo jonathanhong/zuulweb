61891b8b3f31d7b4cc44f6a7d45f2eaafe0d612d
Jonathan Hong <Jonathan.Hong8@gmail.com>
2014-04-02 17:07:46 -0700

    editted js

--- app/assets/javascripts/profile.js
+++ app/assets/javascripts/profile.js
@@ -1,14 +1,15 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://coffeescript.org/
+/* ZuulWeb 2.0 Profile Index profile.js JavaScript Document */
+
+//expands the table row of class .hidden-row below
 function expand() {
 	this.innerHTML = '<span class="glyphicon glyphicon-minus"></span>';
-	#TODO: expand functionality here
-}
+	//TODO: expand functionality here
+};
 
+//sets up the event handlers
 window.onload = function() {
 	var expandBtns = document.getElementsByClass("expand");
 	for (var i = 0; i < expandButtons.length; i++) {
 		expandBtns[i].onclick = expand();
 	}
-}
+};
