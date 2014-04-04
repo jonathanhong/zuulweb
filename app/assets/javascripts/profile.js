0b75c1b631878e0e5997cc33db783c237594ce4a
Jonathan Hong <Jonathan.Hong8@gmail.com>
2014-04-04 14:45:23 -0700

    added expand functionality

--- app/assets/javascripts/profile.js
+++ app/assets/javascripts/profile.js
@@ -2,8 +2,20 @@
 
 //expands the table row of class .hidden-row below
 function expand() {
-	console.log(this);
-	this.innerHTML = '<span class="glyphicon glyphicon-minus"></span>';
+	var $this=$(this),
+	that = $this.parent().parent()[0],
+	isItem = false,
+	$itemRows = $("#transactions > tbody > tr").filter(function() {
+		if (this === that) {
+			isItem = true;
+			return false;
+		} else if (!($(this).hasClass("item")) {
+			isItem = false;
+			return false;
+		}
+	return isItem;
+	}).toggleClass("hidden-row");	
+	$this.find("span").toggleClass("glyphicon-plus").toggleClass("glyphicon-minus");
 	//TODO: expand functionality here
 };
 
