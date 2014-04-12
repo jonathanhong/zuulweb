1a4062c4175c4c357a718029f4b7009b990963f7
Jonathan Hong <Jonathan.Hong8@gmail.com>
2014-04-11 17:13:33 -0700

    fixed db column references in profile

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -2,6 +2,6 @@ class ProfileController < ApplicationController
   def index
 		#TODO: add user auth
 		@user = User.find_by username: 'jhong'
-		@transactions = Transaction.where("userid = ?", @user.id).limit(5).order('created_at desc')	
+		@transactions = User.transactions.limit(5)
   end
 end
