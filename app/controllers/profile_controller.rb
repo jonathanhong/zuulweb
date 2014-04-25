24f2bc5e24bf8c302ea0cb364933d4446f764fab
Jonathan Hong <Jonathan.Hong8@gmail.com>
2014-04-25 15:05:36 -0700

    trying different associations for user transactions

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -2,6 +2,6 @@ class ProfileController < ApplicationController
   def index
 		#TODO: add user auth
 		@user = User.find_by username: 'jhong'
-		@transactions = User.transactions.limit(5)
+		@transactions = @user.transactions.limit(5)
   end
 end
